53872dbda5994a1eb51036d1603a584dabe4c9e8
Sergei <sergei@localhost.localdomain>
2018-08-23 21:05:33 +0300

    Passed last jshint warning

--- mines.js
+++ mines.js
@@ -1,9 +1,9 @@
 ﻿(function () {
-    let minesScript = document.getElementById("minesScript");
-    let mainDiv = document.createElement("div");
+    var minesScript = document.getElementById("minesScript");
+    var mainDiv = document.createElement("div");
     mainDiv.id = "mainDiv";
     document.body.insertBefore(mainDiv, minesScript);
-    let minesPercent = document.getElementById("minesPercent");
+    var minesPercent = document.getElementById("minesPercent");
     minesPercent.value = 10;
     minesPercent.addEventListener("change", function () { GenerateField(); });
     GenerateField();
@@ -11,7 +11,7 @@
 function getButtonAround(a, b) { return document.getElementById("btn_" + a + "_" + b); }
 function CheckBoom(btnClicked) {
     if (!btnClicked.disabled) {
-        let v = parseInt(btnClicked.value);
+        var v = parseInt(btnClicked.value);
         btnClicked.disabled = true;
         if (9 == v) {
             btnClicked.innerHTML = "<img />";
@@ -19,11 +19,11 @@ function CheckBoom(btnClicked) {
         else {
             /*TODO: change later by using Template literals + Babel (IE11 does not support it directly) */
             btnClicked.innerHTML = "<span class=\"sc" + v + "\">" + v + "</span>";
-            let arr = btnClicked.id.split("_");
-            let a = parseInt(arr[1]);
-            let b = parseInt(arr[2]);
+            var arr = btnClicked.id.split("_");
+            var a = parseInt(arr[1]);
+            var b = parseInt(arr[2]);
             if (0 == btnClicked.value) {
-                let btn = getButtonAround(a - 1, b - 1); if (btn) { CheckBoom(btn); }
+                var btn = getButtonAround(a - 1, b - 1); if (btn) { CheckBoom(btn); }
                 btn = getButtonAround(a - 1, b); if (btn) { CheckBoom(btn); }
                 btn = getButtonAround(a - 1, b + 1); if (btn) { CheckBoom(btn); }
                 btn = getButtonAround(a, b - 1); if (btn) { CheckBoom(btn); }
@@ -35,7 +35,7 @@ function CheckBoom(btnClicked) {
         }
     }
 }
-function getMineAround(a, b) {
+function getMineAround(a, b, fieldData) {
     try {
         return (isNaN(fieldData[a][b])) ? 0 : fieldData[a][b];
     }
@@ -44,54 +44,58 @@ function getMineAround(a, b) {
     }
 }
 function GenerateField() {
-    let mainDiv = document.getElementById("mainDiv");
+    var mainDiv = document.getElementById("mainDiv");
     mainDiv.innerHTML = "";
-    let fieldWidth = 10;
-    let fieldHeight = 10;
-    let fieldData = [];
-    for (let i = 0; i < fieldHeight; i++) {
-        let fieldLine = [];
-        for (let j = 0; j < fieldWidth; j++) {
-            fieldLine[j] = 0;
+    var fieldWidth = 10;
+    var fieldHeight = 10;
+    var fieldData = [];
+    for (var i1 = 0; i1 < fieldHeight; i1++) {
+        var fieldLine = [];
+        for (var j1 = 0; j1 < fieldWidth; j1++) {
+            fieldLine[j1] = 0;
         }
-        fieldData[i] = fieldLine;
+        fieldData[i1] = fieldLine;
     }
-    let minesPercent = document.getElementById("minesPercent");
-    let mp = parseInt(minesPercent.value);
-    let minesCount = Math.round(fieldHeight * fieldWidth * (mp / 100));
-    let minesMined = 0;
+    var minesPercent = document.getElementById("minesPercent");
+    var mp = parseInt(minesPercent.value);
+    var minesCount = Math.round(fieldHeight * fieldWidth * (mp / 100));
+    var minesMined = 0;
     while (minesMined < minesCount) {
-        let tmpI = Math.floor(Math.random() * fieldHeight);
-        let tmpJ = Math.floor(Math.random() * fieldWidth);
+        var tmpI = Math.floor(Math.random() * fieldHeight);
+        var tmpJ = Math.floor(Math.random() * fieldWidth);
         if (fieldData[tmpI][tmpJ] == 0) {
             fieldData[tmpI][tmpJ] = 1;
             minesMined++;
         }
     }
-    for (let i = 0; i < fieldHeight; i++) {
-        let p = document.createElement("p");
+    for (var i = 0; i < fieldHeight; i++) {
+        var p = document.createElement("p");
         mainDiv.appendChild(p);
-        for (let j = 0; j < fieldWidth; j++) {
-            let btn = document.createElement("button");
+        for (var j = 0; j < fieldWidth; j++) {
+            var btn = document.createElement("button");
             p.appendChild(btn);
-            let btnId = "btn_" + i + "_" + j;
+            var btnId = "btn_" + i + "_" + j;
             btn.id = btnId;
             if (1 == fieldData[i][j]) {
                 btn.value = 9;
             }
             else {
-                let minesAround = 0;
-                minesAround += getMineAround(i - 1, j - 1);
-                minesAround += getMineAround(i - 1, j);
-                minesAround += getMineAround(i - 1, j + 1);
-                minesAround += getMineAround(i, j - 1);
-                minesAround += getMineAround(i, j + 1);
-                minesAround += getMineAround(i + 1, j - 1);
-                minesAround += getMineAround(i + 1, j);
-                minesAround += getMineAround(i + 1, j + 1);
+                var minesAround = 0;
+                minesAround += getMineAround(i - 1, j - 1, fieldData);
+                minesAround += getMineAround(i - 1, j, fieldData);
+                minesAround += getMineAround(i - 1, j + 1, fieldData);
+                minesAround += getMineAround(i, j - 1, fieldData);
+                minesAround += getMineAround(i, j + 1, fieldData);
+                minesAround += getMineAround(i + 1, j - 1, fieldData);
+                minesAround += getMineAround(i + 1, j, fieldData);
+                minesAround += getMineAround(i + 1, j + 1, fieldData);
                 btn.value = minesAround;
             }
-            btn.addEventListener("click", function () { CheckBoom(btn); });
+            btn.addEventListener("click", ButtonHandler());
         }
     }
 }
+function ButtonHandler() {
+    return function (e) { CheckBoom(e.target); };
+}
+
